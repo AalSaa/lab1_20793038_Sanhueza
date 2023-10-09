b1f19d899bd31edff3175af668401a4787f6fdba
AalSaa <alonso.sanhueza32@gmail.com>
2023-10-09 00:20:41 -0300

    Se ha terminado el archivo de pruebas y se da por terminado el ultimo commit.

--- pruebas_20793038_SanhuezaVega.rkt
+++ pruebas_20793038_SanhuezaVega.rkt
@@ -94,16 +94,16 @@
 
 ; Creacion de chatbot inicial (se intenta agregar varias veces la opcion op101 en el flujo)
 (define op101 (option 1 "1) Equipos" 1 1 "Equipos" "TELEFONO" "celular")) ; option 1/3
-(define op102 (option 1 "2) cambio de PLAN" 2 1 "cambio de PLAN" "plan" "cambio" "actualizar")) ; option 2/3
+(define op102 (option 2 "2) cambio de PLAN" 2 1 "cambio de PLAN" "plan" "cambio" "actualizar")) ; option 2/3
 (define op103 (option 3 "3) ServiCIO tecniCo" 3 1 "ServiCIO tecniCo" "sercivio" "TECNICO")) ; option 3/3
 (define op104 (option 4 "4) PORTABILIDAD" 4 1 "portar" "PORTABILIDAD")) ; option 4/3
 
 (define f101 (flow 1 "Flujo 1 Chatbot0\n¿Qué te gustaría realizar?" op101 op101)) ; flow 1/3
-(define f102 (flow-add-option f101 op101)) ; flow-add-option 1/3
-(define f103 (flow-add-option f101 op102)) ; flow-add-option 2/3
-(define f104 (flow-add-option f101 op103)) ; flow-add-option 3/3
+(define f102 (flow-add-option f101 op102)) ; flow-add-option 1/3
+(define f103 (flow-add-option f102 op103)) ; flow-add-option 2/3
+(define f104 (flow-add-option f103 op104)) ; flow-add-option 3/3
 
-(define cb101 (chatbot 0 "Inicial" "Bienvenido\n¿Qué te gustaría realizar?" 1 f101 f101 f101))  ; chatbot 1/3
+(define cb101 (chatbot 0 "Inicial" "Bienvenido\n¿Qué te gustaría realizar?" 1 f104 f104 f104))  ; chatbot 1/3
 
 ; Creacion de chatbot para equipos (se intenta meter 2 veces el flujo f105 en el chatbot)
 (define op105 (option 1 "1) Comprar" 1 2 "comprar" "obtener" "adquirir")) ; option 5/3
@@ -166,13 +166,44 @@
 (define s109 (system-add-user s108 "user2")) ; system-add-user 2/3
 (define s110 (system-add-user s109 "user1")) ; system-add-user 3/3
 
-; Logear y desloguear usuarios en el sistema (user777 no existe y user1 no se puede loguear hasta desloguear user2)
+; Logear y desloguear usuarios en el sistema 
+; (user777 no existe y user1 no se puede loguear hasta desloguear user2)
 
 (define s111 (system-login s110 "user777")) ; system-login 1/3
 (define s112 (system-login s111 "user2")) ; system-login 2/3
 (define s113 (system-login s112 "user1")) ; system-login 3/3
 (define s114 (system-logout s113)) ; system-logout 1/3
 (define s115 (system-login s114 "USER1")) ; system-login 4/3
+
+; Interactuar con chatbots con system-talk-rec
+
+(define s116 (system-talk-rec s115 "1")) ; system-talk-rec 1/3
+(define s117 (system-talk-rec s116 "3")) ; system-talk-rec 2/3
+(define s118 (system-talk-rec s117 "2")) ; system-talk-rec 3/3
+
+; Intento de interaccion con chatbot sin usuario
+
+(define s119 (system-logout s118)) ; system-logout 2/3
+(define s120 (system-talk-rec s119 "3")) ; system-talk-rec 4/3
+
+; Interactuar con chatbots con system-talk-norec
+
+(define s121 (system-login s120 "user2")) ; system-login 5/3
+(define s122 (system-talk-norec s121 "3")) ; system-talk-norec 1/3
+(define s123 (system-talk-norec s122 "3")) ; system-talk-norec 1/3
+(define s124 (system-talk-norec s123 "4")) ; system-talk-norec 1/3
+
+; Intento de ir a un chatbot a partir de una opcion que no existe
+
+(define s125 (system-talk-norec s124 "9")) ; system-talk-norec 1/3
+
+; Ir del chatbot 4 al chatbot 2 (asumiendo portabilidad en la misma compañia = cambio de plan)
+
+(define s126 (system-talk-norec s125 "2")) ; system-talk-norec 1/3
+
+; ultimo deslogueo (fin de pruebas)
+
+(define s127 (system-logout s126)) ; system-logout 3/3
 
 
 
